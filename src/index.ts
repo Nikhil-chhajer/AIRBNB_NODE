35afcb89b9d8f345534ca831905b7cdb16915de6
Nikhil-chhajer <nikhil.chhajer80@gmail.com>
2025-05-30 16:48:17 +0530

    added hotel model

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import logger from "./config/logger.config";
 import { attachCorrelationIdMiddleware } from "./middlewares/correlation.middleware";
 // import { z } from "zod/v4";
 import { genericErrorHandler } from "./middlewares/error.middleware";
+import sequelize from "./db/models/sequelize";
+// import Hotel from "./db/models/hotel";
 const app=express();
 const PORT=serverconfig.PORT;
 app.use(express.json())
@@ -14,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(attachCorrelationIdMiddleware)
 app.use('/api/v1',v1Router)
 app.use('/api/v2',v2Router)
-app.listen(PORT,()=>{
+app.listen(PORT,async ()=>{
 logger.info("server started at",PORT);
 //whatever we pass in {} in this is taken as data in logger.config file if donot use {} the data obj is empty
 logger.info("hello",{data:"hello nick"})
@@ -28,6 +30,9 @@ app.use(genericErrorHandler)
 //     age:z.number().int().positive()
 // })
 // console.log(objschema.parse(obj));
+    await sequelize.authenticate();
+    logger.info("Database connection has been established successfully.");
+
 });
 
 
